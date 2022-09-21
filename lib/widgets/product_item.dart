e7bcb2432320886d1a82b6bfdcfc090356be09b7
mertguler94 <gulermert94@gmail.com>
2022-09-21 02:43:49 -0400

    Snackbar with undo functionality for item adding to the cart and removing item confirmation is added

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -43,6 +43,16 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             onPressed: (() {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text('Item added to the cart!'),
+                duration: const Duration(seconds: 2),
+                action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    }),
+              ));
             }),
             icon: const Icon(Icons.shopping_cart),
             color: Theme.of(context).colorScheme.secondary,
